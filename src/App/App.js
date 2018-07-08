ea6f79abe91647da702816221e92ecab8050dd8a
OrencioRodolfo <orenciorodolfo@hotmail.com>
2018-07-08 14:15:32 +0100

    refactor(global-style): Remove style from index.html and add injectGlobal in app.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
+import { injectGlobal } from 'styled-components'
 import Home from '../scenes/Home'
 
 const App = props => (
@@ -12,5 +13,19 @@ const App = props => (
 App.propTypes = {
   store: PropTypes.shape({}).isRequired,
 }
+
+/* eslint-disable no-unused-expressions */
+injectGlobal`
+  html,
+  body {
+    margin: 0;
+  }
+  html,
+  body,
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+`
 
 export default App
