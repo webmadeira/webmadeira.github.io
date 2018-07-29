a6ef1bc1858316b1c411cc7d2f9434dea2eb6cc1
Rodolfo Gonçalves <orenciorodolfo@hotmail.com>
2018-07-29 17:11:03 +0100

    chore(storybook): Add storybook setup along with typography stories
    - Add storybook-readme addon
    - Add Knobs addon
    - Add options addon

---
+++ .storybook/config.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { configure, addDecorator } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options'
+import * as theme from '../src/theme'
+
+const req = require.context('../src/', true, /\.stories\.js$/)
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename))
+}
+
+const style = {
+  padding: '20px',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const ThemeDecorator = (storyFn) => (
+  <ThemeProvider theme={theme}>
+    <div style={style}>
+      { storyFn() }
+    </div>
+  </ThemeProvider>
+);
+addDecorator(ThemeDecorator);
+
+setOptions({
+  name: 'WebMadeira',
+  addonPanelInRight: true,
+})
+
+configure(loadStories, module)
