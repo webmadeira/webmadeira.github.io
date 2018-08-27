30a8c4af73eb1f91c1dfea3c793b06d6cbf014eb
Rodolfo Gonçalves <orenciorodolfo@hotmail.com>
2018-08-27 23:03:20 +0100

    feat: Add WebMadeira's event description

--- src/store/actions/event.js
+++ src/store/actions/event.js
@@ -9,7 +9,7 @@ const event2018 = {
   title: '{ "web development talks in Madeira" }',
   logo: 'https://i.imgur.com/sDqnWmE.png',
   date: new Date('2018-12-29'),
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam semper aliquam massa vel aliquam. Duis eleifend fermentum tortor eu pulvinar. Duis maximus mauris nec metus blandit aliquet. Nulla consequat massa a vehicula rutrum. Maecenas et eleifend purus. Aenean maximus ac sem sed mollis. Donec varius lobortis risus, eget bibendum dui vestibulum vel. Ut ullamcorper faucibus porttitor. Curabitur id imperdiet dolor.',
+  description: 'WebMadeira is a non-profit event built by-the-community and for-the-community and you can be part of it. It has born in 2016 and it is back this year with several talks about web development at December 29, 2018. The event should last about 5 to 6 hours holding two kind of talks: "Standard talks" with 20min+5min for Q&A and some very brief ones mentioned as "Lighting talks", taking only 5min and no Q&A. Wanna get on stage? Hit "Become a speaker" button! Wanna help us to maitain this event as non-profit? Hit the "Become a sponsor" button!',
   talks: [
     { id: 0, description: '' },
     { id: 1, description: '' },
